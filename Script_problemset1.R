76a1da4bd23d4d06517f19d7e49dcf5a17bb2e08
GabyFonseca <g.fonsecar@uniandes.edu.co>
2022-10-02 22:41:42 -0500

    Punto 6

--- Script_problemset1.R
+++ Script_problemset1.R
@@ -5,13 +5,14 @@
 
 #Limpiar el entorno
 rm(list=ls())
+##Salida de R :[1] "R version 4.2.1 (2022-06-23 ucrt)"
 
 ##Version de R 
 R.version.string
 
 ## instalar/llamar pacman
 require(pacman)
-p_load(rio, dplyr)
+p_load(rio, dplyr, ggplot2)
 
 ##1. VECTORES
 vector_1 <- c(1:100) 
@@ -50,8 +51,54 @@ data_ocupados %>% select(SECUENCIA_P, ORDEN, HOGAR, DIRECTORIO, ocupados, INGLAB
 ##5.COMBINAR BASE DE DATOS
 data =full_join(data_ocupados, data_personas, by=c("SECUENCIA_P","ORDEN", "HOGAR", "DIRECTORIO"))
 
-
 ##6.DESCRIPTIVAS DE UN CONJUNTO DE DATOS
+
+##Tabla_1. Ingreso segun sexo y departamento. Como es el salario discriminando entre mujeres y hombres, teniendo en cuenta tambien el departamento de donde son 
+data %>% 
+  group_by(P6020, DPTO.y) %>%
+  summarise(prom_inglabo = mean(INGLABO, na.rm=T))
+
+##Tabla_2. Ingreso segun sexo y tipo de contrato
+data %>% 
+  group_by(P6020, P6920) %>%
+  summarise(prom_contrato=mean(INGLABO, na.rm=T))
+
+##Tabla_3. Descritivas de Salario, con ello podemos comparar con las condicionales como cambia el promedio de el ingreso en general vs discriminado por las descritivas condicionales como por sexo
+summary(data[,c("INGLABO")])
+
+##Grafica_1
+ingresos_edad <- data %>% filter(P6040>18 & P6040<70)%>%
+  group_by(P6040) %>% 
+  summarise(ingresos = mean(INGLABO, na.rm = T)) %>% 
+  ggplot(data=. , mapping = aes(x = as.factor(P6040) , y = ingresos, fill = as.factor(P6040))) + 
+  geom_bar(stat = "identity") 
+
+ingresos_edad  +
+  labs(x = "edad") + 
+  theme_classic()
+
+##Grafica_2
+ingresos_pension <- data %>% 
+  group_by(P6920) %>% 
+  summarise(ingresos = mean(INGLABO, na.rm = T)) %>% 
+  ggplot(data=. , mapping = aes(x = as.factor(P6920) , y = ingresos, fill = as.factor(P6920))) + 
+  geom_bar(stat = "identity") 
+
+ingresos_pension  +
+  scale_fill_manual(values = c("1"="#76EEC6" , "2"="#BF3EFF", "3"="#FFE1FF") , label = c("1"="Cotizando" , "2"="No cotizando", "3"="Pensionado") , name = "Cotizante") +
+  labs(x = "Cotizante") + 
+  theme_classic()
+
+##Grafica_3
+Ingreso_edad_sexo = ggplot(data , mapping = aes(x = P6040 , y = INGLABO, group=as.character(P6020), color=as.character(P6020))) +
+  geom_point( size = 0.7)
+
+Ingreso_edad_sexo + scale_color_manual(values = c("2"="slateblue1" , "1"="#EE7600") , label = c("1"="Hombre" , "2"="Mujer") , name = "Sexo")
+
+
+
+
+
 
 
 
